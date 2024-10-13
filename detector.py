0f9b3f6dd1d4508fb4b6a522f7150878ef8829d1
d.koshelev <d.koshelev@zebrains.team>
2024-10-13 10:20:26 +0300

    refactoring

--- detector.py
+++ detector.py
@@ -1,149 +1,116 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 from ultralytics import YOLO
-from ultralytics.utils.plotting import Annotator, colors
 from PIL import Image, ImageDraw, ImageFont
 import time
 
-
 class Detector:
     def __init__(self) -> None:
-        self.yolo_segm = YOLO("./yolo_segm.pt")
-        self.yolo_pixel = YOLO("weights/yolo_pixel.pt")
-        self.yolo_bolt = YOLO("weights/yolo_bolt.pt")
-        self.yolo_scra4 = YOLO("weights/yolo_scra4.pt")
-        self.results = []
+        """Инициализация детектора с моделями YOLO."""
+        self.yolo_segm = YOLO("weights/yolo_segm.pt")  # YOLO для сегментации
+        self.yolo_pixel = YOLO("weights/yolo_pixel.pt")  # YOLO для обнаружения битых пикселей
+        self.yolo_bolt = YOLO("weights/yolo_bolt.pt")  # YOLO для обнаружения болтов
+        self.yolo_scra4 = YOLO("weights/yolo_scra4.pt")  # YOLO для обнаружения царапин
+        self.results = []  # Список для хранения результатов
 
     def _do_segm(self, img: Image.Image):
-        img = img.resize((640, 640))
+        """Выполнение сегментации на входное изображение."""
+        img = img.resize((640, 640))  # Изменение размера изображения
         results = self.yolo_segm.predict(
             img, conf=0.8, iou=0.1, show_labels=True, show_conf=True, imgsz=640
         )
-        img = np.asarray(img, dtype=np.uint8)
+        img = np.asarray(img, dtype=np.uint8)  # Преобразование изображения в массив NumPy
+
+        # Инициализация изображений для разных объектов
+        monirot_img, laptop_img = img.copy(), img.copy()  # По умолчанию оригинальное изображение
+
         if results[0].masks is not None:
             masks = results[0].masks
-            monirot_mask = masks.data.squeeze().cpu().numpy()[0]
-            monirot_mask = (monirot_mask * 255).astype(np.uint8)
+            # Извлечение масок для соответствующих объектов
             try:
+                monirot_mask = (masks.data.squeeze().cpu().numpy()[0] * 255).astype(np.uint8)
                 monirot_img = cv2.bitwise_and(img, img, mask=monirot_mask)
             except:
                 monirot_img = img
+            
 
             if len(results[0].masks) > 1:
-                laptop_mask = masks.data.squeeze().cpu().numpy()[1]
-                laptop_mask = (laptop_mask * 255).astype(np.uint8)
+                laptop_mask = (masks.data.squeeze().cpu().numpy()[1] * 255).astype(np.uint8)
                 laptop_img = cv2.bitwise_and(img, img, mask=laptop_mask)
-            else:
-                laptop_img = img
-        else:
-            monirot_img = img
-            laptop_img = img
 
         return Image.fromarray(laptop_img), Image.fromarray(monirot_img)
 
     def detect(self, imgs: Image.Image):
+        """Обнаружение объектов на входных изображениях."""
+        conf_threshold = 0.2  # Порог достоверности
+        iou_threshold = 0.2  # Порог IoU
 
-        conf_threshold = 0.2
-        iou_threshold = 0.2
         for img in imgs:
-            _results = []
-            laptop_img, monirot_img = self._do_segm(img)
+            _results = []  # Список для хранения результатов для текущего изображения
+            laptop_img, monirot_img = self._do_segm(img)  # Сегментация изображения
 
-            bolt_results = self.yolo_bolt.predict(
-                source=laptop_img,
-                conf=conf_threshold,
-                iou=iou_threshold,
-                show_labels=True,
-                show_conf=True,
-                imgsz=640,
-            )
-            scra4_results = self.yolo_scra4.predict(
-                source=laptop_img,
-                conf=conf_threshold,
-                iou=iou_threshold,
-                show_labels=True,
-                show_conf=True,
-                imgsz=640,
-            )
-            pixels_results = self.yolo_pixel.predict(
-                source=monirot_img,
-                conf=conf_threshold,
-                iou=iou_threshold,
-                show_labels=True,
-                show_conf=True,
-                imgsz=640,
-            )
-            for xy, c, cf, xyn in zip(
-                bolt_results[0].boxes.xyxy,
-                bolt_results[0].boxes.cls,
-                bolt_results[0].boxes.conf,
-                bolt_results[0].boxes.xywhn,
-            ):
+            # Предсказание болтов, царапин и битых пикселей
+            bolt_results = self.yolo_bolt.predict(source=laptop_img, conf=conf_threshold, iou=iou_threshold)
+            scra4_results = self.yolo_scra4.predict(source=laptop_img, conf=conf_threshold, iou=iou_threshold)
+            pixels_results = self.yolo_pixel.predict(source=monirot_img, conf=conf_threshold, iou=iou_threshold)
 
-                _results.append(
-                    [
-                        xy.cpu().numpy(),
-                        c.cpu().numpy(),
-                        cf.cpu().numpy(),
-                        xyn.cpu().numpy(),
-                        bolt_results[0].names[int(c.cpu().numpy())],
-                    ]
-                )
-            for xy, c, cf, xyn in zip(
-                scra4_results[0].boxes.xyxy,
-                scra4_results[0].boxes.cls,
-                scra4_results[0].boxes.conf,
-                scra4_results[0].boxes.xywhn,
-            ):
+            # Извлечение результатов для болтов
+            for xy, c, cf, xyn in zip(bolt_results[0].boxes.xyxy, bolt_results[0].boxes.cls,
+                                       bolt_results[0].boxes.conf, bolt_results[0].boxes.xywhn):
+                _results.append([
+                    xy.cpu().numpy(),
+                    c.cpu().numpy(),
+                    cf.cpu().numpy(),
+                    xyn.cpu().numpy(),
+                    bolt_results[0].names[int(c.cpu().numpy())],
+                ])
 
-                _results.append(
-                    [
-                        xy.cpu().numpy(),
-                        c.cpu().numpy(),
-                        cf.cpu().numpy(),
-                        xyn.cpu().numpy(),
-                        scra4_results[0].names[int(c.cpu().numpy())],
-                    ]
-                )
-            for xy, c, cf, xyn in zip(
-                pixels_results[0].boxes.xyxy,
-                pixels_results[0].boxes.cls,
-                pixels_results[0].boxes.conf,
-                pixels_results[0].boxes.xywhn,
-            ):
+            # Извлечение результатов для царапин
+            for xy, c, cf, xyn in zip(scra4_results[0].boxes.xyxy, scra4_results[0].boxes.cls,
+                                       scra4_results[0].boxes.conf, scra4_results[0].boxes.xywhn):
+                _results.append([
+                    xy.cpu().numpy(),
+                    c.cpu().numpy(),
+                    cf.cpu().numpy(),
+                    xyn.cpu().numpy(),
+                    scra4_results[0].names[int(c.cpu().numpy())],
+                ])
 
-                _results.append(
-                    [
-                        xy.cpu().numpy(),
-                        c.cpu().numpy(),
-                        cf.cpu().numpy(),
-                        xyn.cpu().numpy(),
-                        pixels_results[0].names[int(c.cpu().numpy())],
-                    ]
-                )
-            self.results.append(_results)
+            # Извлечение результатов для битых пикселей
+            for xy, c, cf, xyn in zip(pixels_results[0].boxes.xyxy, pixels_results[0].boxes.cls,
+                                      pixels_results[0].boxes.conf, pixels_results[0].boxes.xywhn):
+                _results.append([
+                    xy.cpu().numpy(),
+                    c.cpu().numpy(),
+                    cf.cpu().numpy(),
+                    xyn.cpu().numpy(),
+                    pixels_results[0].names[int(c.cpu().numpy())],
+                ])
+
+            self.results.append(_results)  # Сохранение результатов для изображения
+
         return bolt_results, scra4_results, pixels_results
 
     def _plot(self, img, bolt_results, scra4_results, pixels_results):
-        bolt_results[0].orig_img = img
-        img = bolt_results[0].plot()
+        """Отображение рамок на изображении."""
+        bolt_results[0].orig_img = img  # Установка оригинального изображения
+        img = bolt_results[0].plot()  # Отображение результатов для болтов
         scra4_results[0].orig_img = img
-        img = scra4_results[0].plot()
+        img = scra4_results[0].plot()  # Отображение результатов для царапин
         pixels_results[0].orig_img = img
-        img = pixels_results[0].plot()
+        img = pixels_results[0].plot()  # Отображение результатов для битых пикселей
         return img
 
     def plot(self, imgs):
-        # pil_img = Image.fromarray(img)
+        """Рисование рамок и меток на изображениях."""
         i = 0
-        _imgs = []
+        _imgs = []  # Список для хранения изображений с рамками
         for img_i, img in enumerate(imgs):
-            if img_i >= len(self.results):
-                result = []
-            else:
-                result = self.results[img_i]
+            # Получение результатов для текущего изображения
+            result = self.results[img_i] if img_i < len(self.results) else []
+
             for xy, c, cf, xyn, name in result:
+                # Рисование рамок и меток на изображении
                 ImageDraw.Draw(img).rectangle(xy, outline="red", width=2)
                 ImageDraw.Draw(img).text(
                     (int(xy[0]), int(xy[1] - 40)),
@@ -152,29 +119,31 @@ class Detector:
                     font=ImageFont.load_default(size=30),
                 )
                 i += 1
-            _imgs.append(img)
+            _imgs.append(img)  # Добавление измененного изображения в список
         return _imgs
 
     def delete_bbox_by_id(self, ids: int):
-        del_indexes = []
+        """Удаление рамок на основе указанных идентификаторов."""
+        del_indexes = []  # Список для хранения индексов удаляемых рамок
         ii = 0
         for i in range(len(self.results)):
             for j in range(len(self.results[i])):
-                if str(ii) in ids:
-                    del_indexes.append([i, j])
+                if str(ii) in ids:  # Проверка, есть ли идентификатор в списке
+                    del_indexes.append([i, j])  # Запись индекса для удаления
                 ii += 1
         for i, j in del_indexes:
-            self.results[i].pop(j)
+            self.results[i].pop(j)  # Удаление рамки из результатов
 
     def get_report(self, laptop_id):
-        res_str = str(self.results)
+        """Генерация отчета на основе результатов обнаружения."""
+        res_str = str(self.results)  # Преобразование результатов в строку
         data = {
             "id": laptop_id,
-            "date": time.time(),
-            "царапина": int("'c'" in res_str),
-            "скол": int("'s'" in res_str),
-            "замок": int("'bad'" in res_str),
-            "отсутсвует болт": int("'empty'" in res_str),
-            "битый пиксель": int("'pixel'" in res_str),
+            "date": time.time(),  # Текющая дата и время
+            "царапина": int("'c'" in res_str),  # Проверка на наличие царапин
+            "скол": int("'s'" in res_str),  # Проверка на наличие сколов
+            "замок": int("'bad'" in res_str),  # Проверка на наличие замков
+            "отсутсвует болт": int("'empty'" in res_str),  # Проверка на отсутствие болтов
+            "битый пиксель": int("'pixel'" in res_str),  # Проверка на битые пиксели
         }
-        return data
+        return data  # Возврат данных отчета
--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from detector import Detector
 import os
 from functions import *
 
+# Initialize the Detector model
+model = Detector()
+
 # Function to train the YOLO model
 def train_yolo(model_name, time_input):
-    # Load the pre-trained model
+    """Load the YOLO model and train it on the user's dataset."""
     model = YOLO("runs/train/yolo11x.pt")
-    # Train the model on the user's dataset
     model.train(
         data="dataset/data.yaml",
         time=time_input,
@@ -20,94 +22,51 @@ def train_yolo(model_name, time_input):
         name=model_name,
     )
 
-
-# Function to predict objects in an image using the YOLO model
+# Function to predict objects in images using the YOLO model
 def predict_image(imgs, text_id):
-    """Predicts objects in an image using a YOLO11 model with adjustable confidence and IOU thresholds."""
-    res = []
-    _imgs = []
-    for img in imgs:
-        img = Image.open(img)
-        img = img.resize((640, 640))
-        _imgs.append(img)
+    """Predicts objects in an image using the YOLO model."""
+    _imgs = [Image.open(img).resize((640, 640)) for img in imgs]
     results = model.detect(_imgs)
     im = model.plot(_imgs)
-
     return im
 
-
+# Function to delete specified detections and re-display images
 def dell(imgs, ids):
+    """Delete detections by ID and re-display images."""
     ids = ids.split(",")
-    res = []
-    _imgs = []
     model.delete_bbox_by_id(ids)
-    for img in imgs:
-        img = Image.open(img)
-        img = img.resize((640, 640))
-        _imgs.append(img)
+    _imgs = [Image.open(img).resize((640, 640)) for img in imgs]
     im = model.plot(_imgs)
     return im
 
-model = Detector()
-
-# Create the Gradio interface for inference
-g = gr.Gallery(type="pil", label="Result")
-grrr = gr.File(
-    file_count="multiple",
-    file_types=["image", "directory"],
-    label="Upload Images or Directory",
-)
-l_id = gr.Textbox(label="Input Laptop ID")
-iface = gr.Interface(
-    fn=predict_image,
-    inputs=[
-        grrr,
-        l_id,
-    ],
-    outputs=[g],
-    title="Детектор дефектов",
-    description="Загругите изображение или несколо изображений",
-)
-def done(l_id,imgs):
+# Function to process confirmed detections and save data
+def done(l_id, imgs):
+    """Confirm detections, save images and annotations."""
     dataset_dir = "dataset"
     data = model.get_report(l_id)
-    save_laptop_data(data,"res.csv")
-    for i,img in enumerate(imgs):
-        if i >= len(model.results):
-            result = []
-        else:
-            result = model.results[i]
-
-        img_name = str(len(os.listdir(os.path.join(dataset_dir,"train/images"))))
+    save_laptop_data(data, "res.csv")
+    
+    for i, img in enumerate(imgs):
+        result = model.results[i] if i < len(model.results) else []
+        img_name = str(len(os.listdir(os.path.join(dataset_dir, "train/images"))))
+        
         img = Image.open(img)
         img = img.resize((640, 640))
-        img.save(os.path.join(dataset_dir,"train/images",f"{img_name}.jpg"))
-        save_annotations(result,os.path.join(dataset_dir,"train/labels",f"{img_name}.txt"))
+        img.save(os.path.join(dataset_dir, "train/images", f"{img_name}.jpg"))
+        save_annotations(result, os.path.join(dataset_dir, "train/labels", f"{img_name}.txt"))
 
+# Function to generate and download the report
 def down_report():
+    """Generate statistics report from saved data and return PDF filename."""
     t = time.time()
     header, data, counts, average = generate_statistics("res.csv", 0, t)
     pdf_filename = 'statistics_report.pdf'
     save_statistics_to_pdf(header, data, counts, average, pdf_filename)
-    return 'statistics_report.pdf'
-
-with gr.Blocks() as demo:
-    iface.render()
-    with gr.Column():
-        input_string = gr.Textbox(label="ID детекций для удаления (1,2,3,4)")
-        process_btn = gr.Button("Удалить выбранные детекции")
-        done_btn = gr.Button("Подтвердить")
-        down_btn = gr.Button("Выгрузить отчет")
-        download_output = gr.File(label="Скачать отчет")
-
-    # Connect the button to the additional function
-    process_btn.click(fn=dell, inputs=[grrr,input_string], outputs=[g])
-    done_btn.click(fn=done,inputs=[l_id,grrr],outputs=None)
-    down_btn.click(fn=down_report,inputs=None,outputs=download_output)
-
+    return pdf_filename
 
 # Function to start or stop the training process
 def start_stop_train(model_name_input, time_input, button_text):
+    """Start or stop the training of the YOLO model."""
     global training_process
     if button_text == "Начать обучение":
         training_process = multiprocessing.Process(
@@ -119,23 +78,46 @@ def start_stop_train(model_name_input, time_input, button_text):
         training_process.terminate()
         return "Обучение завершено", "Начать обучение"
 
+# Create Gradio interface for inference
+with gr.Blocks() as demo:
+    g = gr.Gallery(type="pil", label="Result")
+    grrr = gr.File(
+        file_count="multiple",
+        file_types=["image", "directory"],
+        label="Upload Images or Directory",
+    )
+    l_id = gr.Textbox(label="Input Laptop ID")
+    iface = gr.Interface(
+        fn=predict_image,
+        inputs=[grrr, l_id],
+        outputs=[g],
+        title="Детектор дефектов",
+        description="Загрузите изображение или несколько изображений для детекции",
+    )
 
-# Create the Gradio interface for training
+    # Additional UI components
+    input_string = gr.Textbox(label="ID детекций для удаления (1,2,3,4)")
+    process_btn = gr.Button("Удалить выбранные детекции")
+    done_btn = gr.Button("Подтвердить")
+    down_btn = gr.Button("Выгрузить отчет")
+    download_output = gr.File(label="Скачать отчет")
+
+    # Connect buttons to functions
+    process_btn.click(fn=dell, inputs=[grrr, input_string], outputs=[g])
+    done_btn.click(fn=done, inputs=[l_id, grrr], outputs=None)
+    down_btn.click(fn=down_report, inputs=None, outputs=download_output)
+
+# Create Gradio interface for training
 with gr.Blocks() as iface2:
-    # Create a button with an initial text
     button = gr.Button("Начать обучение")
-
-    # Define the inputs
     model_name_input = gr.Dropdown(
         choices=["Битых пикселей", "Наличия болтов", "Сколов и царапин"],
         label="Модель для детекции",
     )
     time_input = gr.Number(label="Время обучения в часах", minimum=0.5, value=5)
-
-    # Define the output
     output = gr.Textbox(label="Output")
 
-    # Define the function to handle inputs and connect it to the inputs and output
+    # Handle training input and connect it to function
     button.click(
         fn=start_stop_train,
         inputs=[model_name_input, time_input, button],
@@ -143,7 +125,6 @@ with gr.Blocks() as iface2:
     )
 
 # Create a tabbed interface to switch between inference and training
-
 if __name__ == "__main__":
     demo = gr.TabbedInterface([demo, iface2], ["Детекция дефектов", "Обучение моделей"])
     demo.launch(share=False, show_error=True)
